5c997c8e54878d28602463c35c204a843bf41bb2
Kouhei Sutou <kou@clear-code.com>
2016-12-22 13:48:34 +0900

    Set associations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < ApplicationRecord
+  has_many :taggings
+  has_many :tags, through: :taggings
+
   source = DocumentsSearcher.source(self)
   source.title = :title
   source.content = :content
